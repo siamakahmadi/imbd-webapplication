ba666d85c2547a66341440a8c007a2120b0f2ce7
Siamak Ahmadi <64158802+siamakahmadi@users.noreply.github.com>
2021-12-16 12:29:37 +0330

    tree update

--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -10,49 +10,109 @@ export default function Movies() {
           className=" searchInputBg w-full h-full rounded-full "
           placeholder="Type Movie Name"
         />
-        <div className="searchIcon w-24 h-24"></div>
+        <div className="searchIcon w-24 h-24"> </div>
       </div>
 
       <div>
-        <div className="flex justify-between mt-4 items-baseline text-red">
-          <h1 className="text-lg font-semibold" >Today Shows</h1>
+        <div className="flex justify-between mt-4 items-baseline ">
+          <h1 className="text-lg font-semibold">Today Shows</h1>
           <span className="text-sm text-blue-600">See More</span>
         </div>
-        <div className=" flex scroll  ">
-            <div className="bg-indigo-900 w-44 h-auto rounded-md mr-6 w-60 h-70">
-                <div className="w-40 h-30 rounded-md">
-                    <img className="" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-21a1c803fe4ff4b858de24f5c91ec57f_screen.jpg?ts=1636996180" alt="" />
-                </div>
-                <div>
-                    <h1> Mscssovie Title</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,  </p>
-                </div>
-            </div>
 
-            <div className="bg-indigo-900 w-44 h-auto rounded-md mr-6">
-                <div className="w-40 h-40 rounded-md">
-                    <img src="" alt="" />
-                </div>
-                <div>
-                    <h1> Mscssovie Title</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,  </p>
-                </div>
-            </div>
 
-            <div className="bg-indigo-900 w-44 h-auto rounded-md mr-6">
-                <div className="w-40 h-40 rounded-md">
-                    <img src="" alt="" />
-                </div>
-                <div>
-                    <h1> Mscssovie Title</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,  </p>
-                </div>
+
+        <div className=" flex scroll w-full mt-4">
+
+          <div className=" w-44 h-55 rounded-md mr-8 ">
+            <div className="w-40 h-55">
+              <img className=" inset-0 w-full h-full object-cover rounded-xl" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-21a1c803fe4ff4b858de24f5c91ec57f_screen.jpg?ts=1636996180"  alt="" />
             </div>
+            <div className="flex flex-col mt-2">
+              <h1 className="text-md font-bold"> Mscssovie Title</h1>
+              <p className="text-xs mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
+            </div>
+          </div>
+
+
+
+          <div className=" w-44 h-55 rounded-md mr-8 ">
+            <div className="w-40 h-55">
+              <img className=" inset-0 w-full h-full object-cover rounded-xl" src="https://cdn.pastemagazine.com/www/system/images/photo_albums/best-movie-posters-2016/large/moonlight-ver2-xlg.jpg?1384968217"  alt="" />
+            </div>
+            <div className="flex flex-col mt-2">
+              <h1 className="text-md font-bold"> Mscssovie Title</h1>
+              <p className="text-xs mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
+            </div>
+          </div>
+
+          <div className=" w-44 h-55 rounded-md mr-8 ">
+            <div className="w-40 h-55">
+              <img className=" inset-0 w-full h-full object-cover rounded-xl" src="https://www.reeldeals.com/Images/HomeImages/36692.jpg"  alt="" />
+            </div>
+            <div className="flex flex-col mt-2">
+              <h1 className="text-md font-bold"> Mscssovie Title</h1>
+              <p className="text-xs mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
+            </div>
+          </div>
+
+
         </div>
       </div>
 
 
-      
+
+
+
+
+
+      <div>
+        <div className="flex justify-between mt-4 items-baseline ">
+          <h1 className="text-lg font-semibold">Today Shows</h1>
+          <span className="text-sm text-blue-600">See More</span>
+        </div>
+
+
+
+        <div className=" flex scroll w-full mt-4">
+
+          <div className=" w-44 h-55 rounded-md mr-8 ">
+            <div className="w-40 h-55">
+              <img className=" inset-0 w-full h-full object-cover rounded-xl" src="https://maxcdn.icons8.com/app/uploads/2019/05/poster-for-movie.png"  alt="" />
+            </div>
+            <div className="flex flex-col mt-2">
+              <h1 className="text-md font-bold"> Mscssovie Title</h1>
+              <p className="text-xs mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
+            </div>
+          </div>
+
+
+
+          <div className=" w-44 h-55 rounded-md mr-8 ">
+            <div className="w-40 h-55">
+              <img className=" inset-0 w-full h-full object-cover rounded-xl" src="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/169241483/original/af413b406d3cb2eedc149baf2a8b87156d86d56f/make-your-movie-or-short-film-poster.jpg"  alt="" />
+            </div>
+            <div className="flex flex-col mt-2">
+              <h1 className="text-md font-bold"> Mscssovie Title</h1>
+              <p className="text-xs mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
+            </div>
+          </div>
+
+          <div className=" w-44 h-55 rounded-md mr-8 ">
+            <div className="w-40 h-55">
+              <img className=" inset-0 w-full h-full object-cover rounded-xl" src="https://img.buzzfeed.com/buzzfeed-static/static/2020-10/27/21/asset/2eae1b7041a8/sub-buzz-2749-1603833440-24.jpg"  alt="" />
+            </div>
+            <div className="flex flex-col mt-2">
+              <h1 className="text-md font-bold"> Mscssovie Title</h1>
+              <p className="text-xs mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+
+
+
     </div>
   );
 }
