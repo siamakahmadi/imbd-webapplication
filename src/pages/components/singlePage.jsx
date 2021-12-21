8300a1c78ba27873ed12ef84fb213481aad7a539
Siamak Ahmadi <64158802+siamakahmadi@users.noreply.github.com>
2021-12-21 22:26:09 +0330

    Add Single Movie Screen

--- src/pages/components/singlePage.jsx
+++ src/pages/components/singlePage.jsx
@@ -1,26 +1,23 @@
 import React,{useEffect,useState} from "react";
 import { useParams } from 'react-router-dom'
-
+import MovieSinglePage from "./MovieSinglePage";
 
 
 export default function SinglePage() {
-
-
-  const params = useParams().slug;
-  console.log(params);
-
+  const Slug = useParams().slug;
   const [moviesContent, setmoviesContent] = useState([]);
-
   useEffect(()=>{
-    fetch(`http://localhost:3000/recentlymovies/${params}`)
+    fetch(`http://localhost:3000/recentlymovies?Slug=${Slug}`)
     .then(Response=> Response.json())
     .then(movies=>setmoviesContent(movies));
-  }
-  ,[]);
+    
+  },[]);
+  const MoviesDetails = moviesContent.map(detail=> <MovieSinglePage Title={detail.Title} Summary={detail.Summary} Img={detail.Img} Description={detail.Description} Time={detail.Time} Genres={detail.Genres} />) ;
+  // const moviedetail = movies.map(detail=> <MovieCard  title={detail.Title} slug={detail.Slug} image={detail.Img} caption={detail.Summary} />) ;
 
   return (
-    <div>
-      <h1>Welcome To Single Page</h1>
+    <div> 
+      {MoviesDetails}
     </div>
   )
 }
